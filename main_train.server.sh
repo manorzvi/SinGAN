df62ecea2afea8ea2e96a5dff4b9d2454b42d3d6
manorz <manor.zvi@campus.technion.ac.il>
2020-01-22 19:31:49 +0200

    bug fix

--- main_train.server.sh
+++ main_train.server.sh
@@ -12,19 +12,14 @@ python -c 'print("We are harvesting the power of CS faculty for our needs! ya we
 # regular SinGAN
 python main_train.py --input_name nature5.jpg --input_dir Input/People --delete_previous
 # Masking Z reconstruction
-python main_train.py --input_name nature5.jpg --input_dir Input/People --delete_previous -t_m               z_rec_mask -bb (1400,1200),(2200,1900)
+python main_train.py --input_name nature5.jpg --input_dir Input/People --delete_previous -t_m               z_rec_mask -bb "(1400,1200),(2200,1900)"
 
 # Masking disc. map gradients before backprop.
-python main_train.py --input_name nature5.jpg --input_dir Input/People --delete_previous -t_m gradient_mask            -bb (1400,1200),(2200,1900)
+python main_train.py --input_name nature5.jpg --input_dir Input/People --delete_previous -t_m gradient_mask            -bb "(1400,1200),(2200,1900)"
 # Masking disc. output gradients before backprop AND masking Z reconstruction
-python main_train.py --input_name nature5.jpg --input_dir Input/People --delete_previous -t_m gradient_mask z_rec_mask -bb (1400,1200),(2200,1900)
+python main_train.py --input_name nature5.jpg --input_dir Input/People --delete_previous -t_m gradient_mask z_rec_mask -bb "(1400,1200),(2200,1900)"
 
 # Masking disc. map itself before backprop
-python main_train.py --input_name nature5.jpg --input_dir Input/People --delete_previous -t_m disc_map_mask            -bb (1400,1200),(2200,1900)
+python main_train.py --input_name nature5.jpg --input_dir Input/People --delete_previous -t_m disc_map_mask            -bb "(1400,1200),(2200,1900)"
 # Masking disc. map itself before backprop AND masking Z reconstruction
-python main_train.py --input_name nature5.jpg --input_dir Input/People --delete_previous -t_m disc_map_mask z_rec_mask -bb (1400,1200),(2200,1900)
-
-
-
-
-
+python main_train.py --input_name nature5.jpg --input_dir Input/People --delete_previous -t_m disc_map_mask z_rec_mask -bb "(1400,1200),(2200,1900)"
