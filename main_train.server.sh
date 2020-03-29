537d60b58a1574b9cae0055ee77011659017ce10
Manor Zvi <manor.zvi@rishon.csf.technion.ac.il>
2020-03-29 21:11:21 +0300

    add random samples into train script in order to run on gpu + add min_size 15,20

--- main_train.server.sh
+++ main_train.server.sh
@@ -29,6 +29,21 @@ python main_train.py --input_name western_wall.jpg    --input_dir Input/Images -
 python main_train.py --input_name windows.jpg         --input_dir Input/Images --mask_coords "(200,280),(700,760)"     --norm 0 --min_size 50  --max_size 500
 python main_train.py --input_name balloons_large.jpeg --input_dir Input/Images --mask_coords "(330,410),(330,390)"     --norm 0 --min_size 50  --max_size 500
 
-python main_train.py --input_name woman_bricks.jpg    --input_dir Input/Images --mask_coords "(1050,3200),(2600,3500)" --norm 0 --min_size 100 --max_size 1000
-python main_train.py --input_name climb1.jpg          --input_dir Input/Images --mask_coords "(120,290),(800,870)"     --norm 0 --min_size 100 --max_size 1000
-python main_train.py --input_name nature5.jpg         --input_dir Input/Images --mask_coords "(1245,1875),(1495,2080)" --norm 0 --min_size 100 --max_size 1000
+python main_train.py --input_name woman_bricks.jpg    --input_dir Input/Images --mask_coords "(1050,3200),(2600,3500)" --norm 0 --min_size 50 --max_size 500
+python main_train.py --input_name climb1.jpg          --input_dir Input/Images --mask_coords "(120,290),(800,870)"     --norm 0 --min_size 50 --max_size 500
+python main_train.py --input_name nature5.jpg         --input_dir Input/Images --mask_coords "(1245,1875),(1495,2080)" --norm 0 --min_size 50 --max_size 500
+
+python main_train.py --input_name balloons.png        --input_dir Input/Images --mask_coords "(100,130),(100,120)"     --norm 0 --min_size 20
+python main_train.py --input_name balloons.png        --input_dir Input/Images --mask_coords "(100,130),(100,120)"     --norm 0 --min_size 15
+
+python random_samples.py --input_name woman_bricks.jpg    --model_dir TrainedModels/woman_bricks/scale_factor\=0.750000\,alpha\=10\,min_size\=25\,max_size\=250/    --mode random_samples --gen_start_scale 0 --min_size 25 --max_size 250 &
+python random_samples.py --input_name woman_bricks.jpg    --model_dir TrainedModels/woman_bricks/scale_factor\=0.750000\,alpha\=10\,min_size\=25\,max_size\=250/    --mode random_samples --gen_start_scale 1 --min_size 25 --max_size 250 &
+python random_samples.py --input_name balloons_large.jpeg --model_dir TrainedModels/balloons_large./scale_factor\=0.750000\,alpha\=10\,min_size\=50\,max_size\=500/ --mode random_samples --gen_start_scale 0 --min_size 50 --max_size 500 &
+python random_samples.py --input_name balloons_large.jpeg --model_dir TrainedModels/balloons_large./scale_factor\=0.750000\,alpha\=10\,min_size\=50\,max_size\=500/ --mode random_samples --gen_start_scale 1 --min_size 50 --max_size 500 &
+python random_samples.py --input_name western_wall.jpg    --model_dir TrainedModels/western_wall/scale_factor\=0.750000\,alpha\=10\,min_size\=25\,max_size\=250/    --mode random_samples --gen_start_scale 0 --min_size 25 --max_size 250 &
+python random_samples.py --input_name western_wall.jpg    --model_dir TrainedModels/western_wall/scale_factor\=0.750000\,alpha\=10\,min_size\=25\,max_size\=250/    --mode random_samples --gen_start_scale 1 --min_size 25 --max_size 250 &
+python random_samples.py --input_name climb1.jpg          --model_dir TrainedModels/climb1/scale_factor\=0.750000\,alpha\=10\,min_size\=25\,max_size\=250/          --mode random_samples --gen_start_scale 0 --min_size 25 --max_size 250 &
+python random_samples.py --input_name climb1.jpg          --model_dir TrainedModels/climb1/scale_factor\=0.750000\,alpha\=10\,min_size\=25\,max_size\=250/          --mode random_samples --gen_start_scale 1 --min_size 25 --max_size 250 &
+
+
+
