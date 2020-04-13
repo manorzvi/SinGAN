3c7b057e29c01c5200bace9ba619dcd21437fc4a
manorz <manor.zvi@campus.technion.ac.il>
2020-04-13 22:05:02 +0300

    for training with the gradient panelty feature

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -126,6 +126,8 @@ def train_single_scale(netD,netG,reals,masks,Gs,Zs,in_s,NoiseAmp,opt,centers=Non
     norm.append((h*w)/discriminators_mask.sum().item())
     norm.append(1) #placeholder
 
+    plt.imsave('%s/masked_img.png'   % (opt.outf), functions.convert_image_np(real*mask))
+
     for epoch in range(opt.niter):
         if (Gs == []) & (opt.mode != 'SR_train'):
             if (epoch == 0):
@@ -264,7 +266,7 @@ def train_single_scale(netD,netG,reals,masks,Gs,Zs,in_s,NoiseAmp,opt,centers=Non
         schedulerD.step()
         schedulerG.step()
 
-    plt.imsave('%s/masked_img.png'   % (opt.outf), functions.convert_image_np(real*mask))
+    # plt.imsave('%s/masked_img.png'   % (opt.outf), functions.convert_image_np(real*mask))
     functions.save_networks(netG,netD,z_opt,opt)
     return z_opt,in_s,netG
 
