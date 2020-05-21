71acde6d39bf0258282276ec332dd212ffa954a7
manorzvi <manor.zvi@campus.technion.ac.il>
2020-05-21 08:08:18 +0300

    stochastic gen script

---
+++ random_samples-stoch_gen.sh
@@ -0,0 +1,9 @@
+random_samples.py --input_name woman_bricks.jpg --model_dir TrainedModels/woman_bricks/scale_factor\=0.750000\,alpha\=10\,min_size\=25\,max_size\=250/ --mode random_samples --gen_start_scale 1 --min_size 25 --max_size 250 --mask_coords (1050,3200),(2600,3500)
+random_samples.py --input_name western_wall.jpg --model_dir TrainedModels/western_wall/scale_factor\=0.750000\,alpha\=10\,min_size\=25\,max_size\=250/ --mode random_samples --gen_start_scale 1 --min_size 25 --max_size 250 --mask_coords (370,630),(480,600)
+random_samples.py --input_name windows.jpg      --model_dir TrainedModels/windows/scale_factor\=0.750000\,alpha\=10\,min_size\=25\,max_size\=250/      --mode random_samples --gen_start_scale 1 --min_size 25 --max_size 250 --mask_coords (200,280),(700,760)
+random_samples.py
+random_samples.py
+random_samples.py
+random_samples.py
+random_samples.py
+random_samples.py
